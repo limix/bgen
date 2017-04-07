8b15adb5916b0a0fb10350426809696a81fbd937
Danilo Horta <danilo.horta@gmail.com>
2017-04-07 11:16:17 +0100

    80

--- bgen_reader/bgen_reader.c
+++ bgen_reader/bgen_reader.c
@@ -5,143 +5,143 @@
 #include <stdlib.h>
 
 int64_t fread_check(void *restrict buffer, size_t size,
-                    FILE *restrict stream, char* filepath)
+                    FILE *restrict stream, char *filepath)
 {
-        size_t err = fread(buffer, size, 1, stream);
-        if (err != 1)
+    size_t err = fread(buffer, size, 1, stream);
+
+    if (err != 1)
+    {
+        if (feof(stream))
         {
-                if (feof(stream))
-                {
-                        fprintf(stderr, "Error reading %s: unexpected end of file.\n", filepath);
-                        return -1;
-                }
-                fprintf(stderr, "Unknown error while reading %s.\n", filepath);
-                return -1;
+            fprintf(stderr,
+                    "Error reading %s: unexpected end of file.\n",
+                    filepath);
+            return -1;
         }
-        return 0;
+        fprintf(stderr, "Unknown error while reading %s.\n", filepath);
+        return -1;
+    }
+    return 0;
 }
 
 typedef struct
 {
-        uint32_t offset;
-        uint32_t header_length;
-        uint32_t nvariants;
-        uint32_t nsamples;
-        uint32_t magic_number;
-        uint32_t flags;
+    uint32_t offset;
+    uint32_t header_length;
+    uint32_t nvariants;
+    uint32_t nsamples;
+    uint32_t magic_number;
+    uint32_t flags;
 } Header;
 
-int64_t read_header(Header* header, FILE *restrict f, char* filepath)
+int64_t read_header(Header *header, FILE *restrict f, char *filepath)
 {
+    if (fread_check(&(header->header_length), 4, f, filepath)) return -1;
 
-        if (fread_check(&(header->header_length), 4, f, filepath))
-                return -1;
+    if (fread_check(&(header->nvariants), 4, f, filepath)) return -1;
 
-        if (fread_check(&(header->nvariants), 4, f, filepath))
-                return -1;
+    if (fread_check(&(header->nsamples), 4, f, filepath)) return -1;
 
-        if (fread_check(&(header->nsamples), 4, f, filepath))
-                return -1;
+    if (fread_check(&(header->magic_number), 4, f, filepath)) return -1;
 
-        if (fread_check(&(header->magic_number), 4, f, filepath))
-                return -1;
+    fseek(f, (header->header_length) - 20, SEEK_CUR);
 
-        fseek(f, (header->header_length) - 20, SEEK_CUR);
+    if (fread_check(&(header->flags), 4, f, filepath)) return -1;
 
-        if (fread_check(&(header->flags), 4, f, filepath))
-                return -1;
+    printf("header_length: %u\n", (header->header_length));
+    printf("nvariants: %u\n",     (header->nvariants));
+    printf("nsamples: %u\n",      (header->nsamples));
+    printf("magic_number: %u\n",  (header->magic_number));
+    printf("flags: %u\n",         (header->flags));
 
-        printf("header_length: %u\n", (header->header_length));
-        printf("nvariants: %u\n", (header->nvariants));
-        printf("nsamples: %u\n", (header->nsamples));
-        printf("magic_number: %u\n", (header->magic_number));
-        printf("flags: %u\n", (header->flags));
-
-        return 0;
+    return 0;
 }
 
-int snp_block_compression(Header* header)
+int snp_block_compression(Header *header)
 {
-  return header->flags & 3;
+    return header->flags & 3;
 }
 
-int snp_block_layout(Header* header)
+int snp_block_layout(Header *header)
 {
-  return (header->flags & (4 + 8 + 16 + 32)) >> 2;
+    return (header->flags & (4 + 8 + 16 + 32)) >> 2;
 }
 
-int has_sample_identifier(Header* header)
+int has_sample_identifier(Header *header)
 {
-  return (header->flags & -1) >> 31;
+    return (header->flags & - 1) >> 31;
 }
 
 typedef struct
 {
-  uint16_t length;
-  char* id;
+    uint16_t length;
+    char    *id;
 } SampleId;
 
 typedef struct
 {
-    uint32_t length;
-    uint32_t nsamples;
-    SampleId* sampleids;
+    uint32_t  length;
+    uint32_t  nsamples;
+    SampleId *sampleids;
 } SampleIdBlock;
 
-int read_sample_identifier_block(SampleIdBlock* block, FILE *restrict f, char* filepath)
+int read_sample_identifier_block(SampleIdBlock *block,
+                                 FILE *restrict f,
+                                 char          *filepath)
 {
-  if (fread_check(&(block->length), 4, f, filepath))
-                return -1;
-  if (fread_check(&(block->nsamples), 4, f, filepath))
-                return -1;
+    if (fread_check(&(block->length), 4, f, filepath)) return -1;
 
-  printf("sampleid_length: %d\n", block->length);
-  printf("sampleid_nsamples: %d\n", block->nsamples);
+    if (fread_check(&(block->nsamples), 4, f, filepath)) return -1;
 
-  block->sampleids = malloc(block->nsamples * sizeof(SampleId));
+    printf("sampleid_length: %d\n",   block->length);
+    printf("sampleid_nsamples: %d\n", block->nsamples);
 
-  assert(sizeof(char) == 1);
+    block->sampleids = malloc(block->nsamples * sizeof(SampleId));
 
-  for (size_t i = 0; i < block->nsamples; i++)
-  {
-    if (fread_check(&(block->sampleids[i].length), 2, f, filepath))
-                return -1;
+    assert(sizeof(char) == 1);
 
-    block->sampleids[i].id = malloc(block->sampleids[i].length * sizeof(char));
-    if (fread_check(block->sampleids[i].id, block->sampleids[i].length, f, filepath))
-              return -1;
+    for (size_t i = 0; i < block->nsamples; i++)
+    {
+        uint16_t *length = &(block->sampleids[i].length);
 
-  }
-  return 0;
+        if (fread_check(length, 2, f, filepath)) return -1;
+
+
+        block->sampleids[i].id =
+            malloc(block->sampleids[i].length * sizeof(char));
+
+        if (fread_check(block->sampleids[i].id, block->sampleids[i].length, f,
+                        filepath)) return -1;
+    }
+    return 0;
 }
 
 int bgen_reader_read()
 {
-        char* filepath = "/Users/horta/workspace/bgen-reader/test/example.1bits.bgen";
-        FILE* f = fopen(filepath, "rb");
+    char *fp = "/Users/horta/workspace/bgen-reader/test/example.1bits.bgen";
+    FILE *f  = fopen(fp, "rb");
 
-        uint32_t offset;
+    uint32_t offset;
 
-        if (fread_check(&offset, 4, f, filepath))
-                return -1;
+    if (fread_check(&offset, 4, f, fp)) return -1;
 
-        printf("offset: %u\n", offset);
+    printf("offset: %u\n", offset);
 
-        Header header;
+    Header header;
 
-        if (read_header(&header, f, filepath))
-                return -1;
+    if (read_header(&header, f, fp)) return -1;
 
 
-        printf("snp_block_compression: %d\n", snp_block_compression(&header));
-        printf("snp_block_layout: %d\n", snp_block_layout(&header));
-        printf("has_sample_identifier: %d\n", has_sample_identifier(&header));
+    printf("snp_block_compression: %d\n", snp_block_compression(&header));
+    printf("snp_block_layout: %d\n",      snp_block_layout(&header));
+    printf("has_sample_identifier: %d\n", has_sample_identifier(&header));
 
-        SampleIdBlock sampleid_block;
-        if (has_sample_identifier(&header))
-          if (read_sample_identifier_block(&sampleid_block, f, filepath))
-            return -1;
+    SampleIdBlock sampleid_block;
 
-        fclose(f);
-        return 0;
+    if (has_sample_identifier(&header))
+        if (read_sample_identifier_block(&sampleid_block, f, fp)) return -1;
+
+
+    fclose(f);
+    return 0;
 }
