2f90bbcbb0ac6eb9d1edc10d1ae09e7e6328c09b
Danilo Horta <danilo.horta@gmail.com>
2017-06-12 13:51:21 +0100

    three tests

--- test/test_bgen_reader.c
+++ test/test_bgen_reader.c
@@ -118,11 +118,13 @@ int main()
     bgen_reader_genotype_block(&bgen_file, 0, &vb, ui_probs);
 
     // Sample 0 (sample_001)
-    if (ui_probs[0] != 0 || ui_probs[0] != 0) return EXIT_FAILURE;
+    if ((ui_probs[0] != 0) || (ui_probs[0] != 0)) return EXIT_FAILURE;
+
     // Sample 3 (sample_004)
-    if (ui_probs[6] != 0 || ui_probs[7] != 1) return EXIT_FAILURE;
+    if ((ui_probs[6] != 0) || (ui_probs[7] != 1)) return EXIT_FAILURE;
+
     // Sample 498 (sample_499)
-    if (ui_probs[498 * 2] != 1 || ui_probs[498 * 2 + 1] != 0) return EXIT_FAILURE;
+    if ((ui_probs[498 * 2] != 1) || (ui_probs[498 * 2 + 1] != 0)) return EXIT_FAILURE;
 
     free(ui_probs);
 
@@ -131,49 +133,15 @@ int main()
     // second SNP
     bgen_reader_genotype_block(&bgen_file, 1, &vb, ui_probs);
 
-    if (ui_probs[0] != 0 || ui_probs[0] != 0) return EXIT_FAILURE;
-    if (ui_probs[4] != 1 || ui_probs[5] != 0) return EXIT_FAILURE;
+    if ((ui_probs[0] != 0) || (ui_probs[0] != 0)) return EXIT_FAILURE;
+
+    if ((ui_probs[4] != 1) || (ui_probs[5] != 0)) return EXIT_FAILURE;
+
+    if ((ui_probs[12] != 0) || (ui_probs[13] != 1)) return EXIT_FAILURE;
 
     free(ui_probs);
 
-
-    // // Sample 498 (sample_499)
-    // printf("ui_probs: %u %u\n", up[498 * 2], up[498 * 2 + 1]);
-
-    // printf("ui_probs: %u %u\n", ui_probs[0], ui_probs[1]);
-    // printf("ui_probs: %u %u\n", ui_probs[2], ui_probs[3]);
-    // printf("ui_probs: %u %u\n", ui_probs[4], ui_probs[5]);
-    // printf("ui_probs: %u %u\n", ui_probs[6], ui_probs[7]);
-    // printf("ui_probs: %u %u\n", ui_probs[8], ui_probs[9]);
-    // printf("ui_probs: %u %u\n", ui_probs[10], ui_probs[11]);
-
-    // In [15]: d['genotype'].iloc[3:6, :5]
-    // Out[15]:
-    //     snp_id   rs_id   pos alleleA alleleB
-    //
-    // 1  SNPID_5  RSID_5  5000       A       G
-    // 1  SNPID_6  RSID_6  6000       A       G
-    // 1  SNPID_7  RSID_7  7000       A       G
-
-    // In [21]: d['genotype'].iloc[0:2, 5:20]
-    // Out[21]:
-    //   sample_001                     sample_002                     sample_003  \
-    //           AA        AB        BB         AA        AB        BB         AA
-    // 1   0.000000  0.000000  0.000000   0.027801  0.008636  0.963531   0.017365
-    // 1   0.005066  0.002075  0.992859   0.009644  0.002686  0.987671   0.979706
-    //
-    //                       sample_004                     sample_005            \
-    //          AB        BB         AA        AB        BB         AA        AB
-    // 1  0.049683  0.932922   0.024872  0.932831  0.042297   0.034607  0.019196
-    // 1  0.019470  0.000824   0.981934  0.005310  0.012787   0.007202  0.984406
-    //
-    //
-    //          BB
-    // 1  0.946198
-    // 1  0.008423
-
     bgen_reader_close(&bgen_file);
-
 
 
     return EXIT_SUCCESS;
