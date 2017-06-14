63479d143cc1d9f35b01801eed83c2b7ca6a2744
Danilo Horta <danilo.horta@gmail.com>
2017-06-14 23:42:34 +0100

    propoer zlib usage

--- bgen_reader/util/zlib_wrapper.h
+++ bgen_reader/util/zlib_wrapper.h
@@ -9,20 +9,67 @@
 inline static inti zlib_uncompress(const byte *src, inti src_size,
                                    byte **dst, inti *dst_size)
 {
-    uLongf z_dst_size = *dst_size;
+    int e;
+    z_stream strm;
 
-    int err = uncompress(*dst, &z_dst_size, src, src_size);
+    strm.zalloc   = Z_NULL;
+    strm.zfree    = Z_NULL;
+    strm.opaque   = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in  = Z_NULL;
+    e             = inflateInit(&strm);
 
-    if (err != Z_OK)
+    if (e != Z_OK)
     {
-        printf("zlib failed to uncompress: %s.\n", zError(err));
+        printf("zlib failed to uncompress: %s.\n", zError(e));
         return EXIT_FAILURE;
     }
 
-    *dst_size = z_dst_size;
+    strm.next_in  = (byte *)src;
+    strm.avail_in = src_size;
 
-    *dst = realloc(*dst, *dst_size);
+    strm.avail_out = *dst_size;
+    strm.next_out  = *dst;
 
+    e = inflate(&strm, Z_NO_FLUSH);
+    assert(e != Z_STREAM_ERROR); /* state not clobbered */
+
+    switch (e) {
+    case Z_NEED_DICT:
+        e = Z_DATA_ERROR; /* and fall through */
+
+    case Z_DATA_ERROR:
+    case Z_MEM_ERROR:
+        inflateEnd(&strm);
+        printf("zlib failed to uncompress: %s.\n", zError(e));
+        return EXIT_FAILURE;
+    }
+
+    unsigned int have = *dst_size - strm.avail_out;
+    assert(strm.avail_out == 0);
+
+    if (have > 0)
+    {
+        printf("zlib failed to uncompress.\n");
+        inflateEnd(&strm);
+        return EXIT_FAILURE;
+    }
+
+    // uLongf z_dst_size = *dst_size;
+
+    // int e = uncompress(*dst, &z_dst_size, src, src_size);
+
+    // if (e != Z_OK)
+    // {
+    //     printf("zlib failed to uncompress: %s.\n", zError(e));
+    //     return EXIT_FAILURE;
+    // }
+    //
+    // *dst_size = z_dst_size;
+
+    // *dst = realloc(*dst, *dst_size);
+
+    inflateEnd(&strm);
     return EXIT_SUCCESS;
 }
 
