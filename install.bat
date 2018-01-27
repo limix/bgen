d3bf084a701cbb88b4836f972fcd8bcf6ffa903f
Danilo Horta <danilo.horta@gmail.com>
2018-01-27 18:36:48 +0000

    ARCH

--- install.bat
+++ install.bat
@@ -7,8 +7,9 @@ set /p VERSION=<VERSION.tmp && del VERSION.tmp
 set FILE=bgen-%VERSION%.zip
 set DIR=bgen-%VERSION%
 set URL=https://github.com/limix/bgen/archive/%VERSION%.zip
+IF "%ARCH%"=="" set ARCH=x64
 
-echo [0/5] Library(bgen==%VERSION%)
+echo [0/4] Library(bgen==%VERSION%)
 
 SET ORIGIN=%cd%
 call :joinpath "%ORIGIN%" "install.log"
@@ -20,22 +21,22 @@ rd /S /Q %DIR% >nul 2>&1
 del /Q %LOG_FILE% ! >nul 2>&1
 copy /y nul %LOG_FILE% >nul 2>&1
 
-echo|set /p="[1/5] Downloading... "
+echo|set /p="[1/4] Downloading... "
 echo Fetching %URL% >>%LOG_FILE% 2>&1
 powershell -Command "(New-Object Net.WebClient).DownloadFile('%URL%', '%FILE%')" >>%LOG_FILE% 2>&1
 if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
 
-echo|set /p="[2/5] Extracting... "
+echo|set /p="[2/4] Extracting... "
 powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('%FILE%', '.'); }"
 if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
 
 cd %DIR% && mkdir build && cd build
 
-echo|set /p="[3/5] Configuring... "
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%programfiles%\bgen" >>%LOG_FILE% 2>&1
+echo|set /p="[3/4] Configuring... "
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_GENERATOR_PLATFORM=%ARCH% -DCMAKE_INSTALL_PREFIX="%programfiles%\bgen" >>%LOG_FILE% 2>&1
 if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
 
-echo|set /p="[5/5] Compiling and installing... "
+echo|set /p="[4/4] Compiling and installing... "
 cmake --build . --config Release --target install >>%LOG_FILE% 2>&1
 if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
 
