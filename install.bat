3131a37c0565f0177200a3b98178ffa51795d342
Danilo Horta <danilo.horta@gmail.com>
2018-02-28 17:50:16 +0000

    new windows downloader

--- install.bat
+++ install.bat
@@ -1,9 +1,15 @@
 @echo off
+SETLOCAL
+
+:: Log file
+SET ORIGIN=%cd%
+call :joinpath "%ORIGIN%" "install.log"
+SET LOG_FILE=%Result%
 
 :: Configuration
 set VERSION_URL=https://raw.githubusercontent.com/limix/bgen/master/VERSION
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%VERSION_URL%', 'VERSION.tmp')"
-set /p VERSION=<VERSION.tmp && del VERSION.tmp
+call :winget "%VERSION_URL%" >>%LOG_FILE% 2>&1
+set /p VERSION=<VERSION && del VERSION
 set FILE=bgen-%VERSION%.zip
 set DIR=bgen-%VERSION%
 set URL=https://github.com/limix/bgen/archive/%VERSION%.zip
@@ -23,7 +29,7 @@ copy /y nul %LOG_FILE% >nul 2>&1
 
 echo|set /p="[1/4] Downloading... "
 echo Fetching %URL% >>%LOG_FILE% 2>&1
-powershell -Command "(New-Object Net.WebClient).DownloadFile('%URL%', '%FILE%')" >>%LOG_FILE% 2>&1
+call :winget "%URL%" >>%LOG_FILE% 2>&1
 if %ERRORLEVEL% NEQ 0 (echo FAILED. && type %LOG_FILE% && exit /B 1) else (echo done.)
 
 echo|set /p="[2/4] Extracting... "
@@ -54,3 +60,33 @@ set Path1=%~1
 set Path2=%~2
 if {%Path1:~-1,1%}=={\} (set Result=%Path1%%Path2%) else (set Result=%Path1%\%Path2%)
 goto :eof
+
+:winget    - download file given url
+set URL=%~1
+for %%F in (%URL%) do set FILE=%%~nxF
+
+setlocal EnableDelayedExpansion
+set multiLine=$security_protcols = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::SystemDefault^
+
+if ([Net.SecurityProtocolType].GetMember('Tls11').Count -gt 0) {^
+
+ $security_protcols = $security_protcols -bor [Net.SecurityProtocolType]::Tls11^
+
+}^
+
+if ([Net.SecurityProtocolType].GetMember('Tls12').Count -gt 0) {^
+
+$security_protcols = $security_protcols -bor [Net.SecurityProtocolType]::Tls12^
+
+}^
+
+[Net.ServicePointManager]::SecurityProtocol = $security_protcols^
+
+Write-Host 'Downloading %URL%... ' -NoNewLine^
+
+(New-Object Net.WebClient).DownloadFile('%URL%', '%FILE%')^
+
+Write-Host 'done.'
+
+powershell -Command !multiLine!
+goto:eof
