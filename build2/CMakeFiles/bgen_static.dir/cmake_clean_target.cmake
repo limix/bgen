2719b65269c974610a6ed6c76b88faad148cbd69
Danilo Horta <danilo.horta@gmail.com>
2019-01-01 20:27:44 +0000

    deloete build2

--- build2/CMakeFiles/bgen_static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libbgen_static.a"
-)
--- build2/include/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /bgen/include
-# Build directory: /bgen/build2/include
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
