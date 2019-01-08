dd025534f0826203408112524b06f15ce54a5a51
Danilo Horta <danilo.horta@gmail.com>
2019-01-08 11:48:31 +0000

    reading probabilities for interface 3, loading metafile

--- test/assert_interface_3.c
+++ test/assert_interface_3.c
@@ -133,7 +133,85 @@ int test_genotype_haplotypes_by_creating_metadata()
                     assert_almost_equal(*p, *(probs_ptr++));
                     ++p;
                 }
-                printf("\n");
+            }
+            free(ptr);
+
+            bgen_close_genotype(vg);
+        }
+        bgen_free_partition(vm, nvars);
+    }
+
+    assert_equal_int(bgen_close_metafile(mf), 0);
+    bgen_close(bgen);
+    return 0;
+}
+
+int test_genotype_haplotypes_by_loading_metadata()
+{
+    struct bgen_file *bgen = bgen_open("data/haplotypes.bgen");
+
+    struct bgen_mf *mf = bgen_open_metafile("haplotypes.bgen.metadata.2");
+    assert_not_null(mf);
+
+    assert_equal_int(bgen_metafile_nparts(mf), 4);
+    assert_equal_int(bgen_metafile_nvars(mf), 4);
+
+    int nalleles[] = {2, 2, 2, 2};
+    int min_ploidy[] = {2, 2, 2, 2};
+    int max_ploidy[] = {2, 2, 2, 2};
+    int ncombs[] = {4, 4, 4, 4};
+    int phased[] = {1, 1, 1, 1};
+    int nsamples = 4;
+    double probs[] = {
+        1.00000000000000000000, 0.00000000000000000000, 1.00000000000000000000,
+        0.00000000000000000000, 0.00000000000000000000, 1.00000000000000000000,
+        1.00000000000000000000, 0.00000000000000000000, 1.00000000000000000000,
+        0.00000000000000000000, 0.00000000000000000000, 1.00000000000000000000,
+        0.00000000000000000000, 1.00000000000000000000, 0.00000000000000000000,
+        1.00000000000000000000, 0.00000000000000000000, 1.00000000000000000000,
+        1.00000000000000000000, 0.00000000000000000000, 1.00000000000000000000,
+        0.00000000000000000000, 0.00000000000000000000, 1.00000000000000000000,
+        0.00000000000000000000, 1.00000000000000000000, 0.00000000000000000000,
+        1.00000000000000000000, 1.00000000000000000000, 0.00000000000000000000,
+        1.00000000000000000000, 0.00000000000000000000, 1.00000000000000000000,
+        0.00000000000000000000, 0.00000000000000000000, 1.00000000000000000000,
+        0.00000000000000000000, 1.00000000000000000000, 0.00000000000000000000,
+        1.00000000000000000000, 1.00000000000000000000, 0.00000000000000000000,
+        1.00000000000000000000, 0.00000000000000000000, 0.00000000000000000000,
+        1.00000000000000000000, 1.00000000000000000000, 0.00000000000000000000,
+        0.00000000000000000000, 1.00000000000000000000, 0.00000000000000000000,
+        1.00000000000000000000, 1.00000000000000000000, 0.00000000000000000000,
+        1.00000000000000000000, 0.00000000000000000000, 0.00000000000000000000,
+        1.00000000000000000000, 1.00000000000000000000, 0.00000000000000000000,
+        1.00000000000000000000, 0.00000000000000000000, 0.00000000000000000000,
+        1.00000000000000000000};
+    double *probs_ptr = &probs[0];
+    for (size_t i = 0; i < 4; ++i) {
+        int nvars;
+        struct bgen_vm *vm = bgen_read_partition(mf, i, &nvars);
+
+        for (int ii = 0; ii < nvars; ++ii) {
+            struct bgen_vg *vg = bgen_open_genotype(bgen, vm + ii);
+            assert_not_null(vg);
+
+            assert_equal_int(bgen_nalleles(vg), nalleles[i]);
+            for (size_t j = 0; j < (size_t)nsamples; ++j) {
+                assert_equal_int(bgen_missing(vg, j), 0);
+                assert_equal_int(bgen_ploidy(vg, j), 2);
+            }
+            assert_equal_int(bgen_min_ploidy(vg), min_ploidy[i]);
+            assert_equal_int(bgen_max_ploidy(vg), max_ploidy[i]);
+            assert_equal_int(bgen_ncombs(vg), ncombs[i]);
+            assert_equal_int(bgen_phased(vg), phased[i]);
+
+            double *ptr = malloc(nsamples * ncombs[i] * sizeof(double));
+            assert_equal_int(bgen_read_genotype(bgen, vg, ptr), 0);
+            double *p = ptr;
+            for (int j = 0; j < nsamples; ++j) {
+                for (int c = 0; c < ncombs[i]; ++c) {
+                    assert_almost_equal(*p, *(probs_ptr++));
+                    ++p;
+                }
             }
             free(ptr);
 
@@ -154,6 +232,7 @@ int main()
     TEST_CASE(test_bgen_file_haplotypes());
     TEST_CASE(test_create_meteadata_haplotypes());
     TEST_CASE(test_genotype_haplotypes_by_creating_metadata());
+    TEST_CASE(test_genotype_haplotypes_by_loading_metadata());
 
     return 0;
 }
