0932b8e912a30aa618e69a51160c9c36badddd99
Danilo Horta <danilo.horta@gmail.com>
2019-01-15 13:57:40 +0000

    conf for readthe docs

--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 
 import os
+import subprocess
+import sys
 
 import sphinx_rtd_theme
 
@@ -30,3 +32,29 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 htmlhelp_basename = "bgendoc"
+
+
+def run_doxygen(folder):
+    """Run the doxygen make command in the designated folder"""
+
+    try:
+        retcode = subprocess.call("cd %s; make" % folder, shell=True)
+        if retcode < 0:
+            sys.stderr.write("doxygen terminated by signal %s" % (-retcode))
+    except OSError as e:
+        sys.stderr.write("doxygen execution failed: %s" % e)
+
+
+def generate_doxygen_xml(app):
+    """Run the doxygen make commands if we're on the ReadTheDocs server"""
+
+    read_the_docs_build = os.environ.get("READTHEDOCS", None) == "True"
+
+    if read_the_docs_build:
+        run_doxygen(".")
+
+
+def setup(app):
+
+    # Add hook for building doxygen xml when needed
+    app.connect("builder-inited", generate_doxygen_xml)
