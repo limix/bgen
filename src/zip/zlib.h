bb894857f943dd4b1a87a04e843aa60578f02231
Danilo Horta <danilo.horta@gmail.com>
2019-01-17 00:33:40 +0000

    working on centos5 because o static_assert

--- src/zip/zlib.h
+++ src/zip/zlib.h
@@ -5,6 +5,9 @@
 #include <stddef.h>
 #include <zlib.h>
 
+#ifndef static_assert
+#define static_assert _Static_assert
+#endif
 static_assert(sizeof(size_t) >= sizeof(uInt), "Safe conversion for zlib.");
 
 int bgen_unzlib(const char *, size_t, char **, size_t *);
