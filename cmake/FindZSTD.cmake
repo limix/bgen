085140e51bf59c247e25f1da31f8a3ca626c9242
Danilo Horta <danilo.horta@pm.me>
2020-09-03 12:32:02 +0100

    find static folder

--- cmake/FindZSTD.cmake
+++ cmake/FindZSTD.cmake
@@ -26,57 +26,48 @@
 #   ZSTD_FOUND       - True if zstd library is found.
 
 
-message("Ponto 1")
 set(zstd_incl_dirs "/usr/include" "/usr/local/include")
 if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
     set(zstd_incl_dirs ${zstd_incl_dirs} "$ENV{PROGRAMFILES}/zstd/include")
     set(zstd_incl_dirs ${zstd_incl_dirs} "$ENV{PROGRAMDATA}/chocolatey/lib/zstandard/tools/include")
 endif()
 
-message("Ponto 2")
 if(ZSTD_INCLUDEDIR)
     list(APPEND zstd_incl_dirs ${ZSTD_INCLUDEDIR})
 endif()
 
-message("Ponto 3")
 find_path(
     ZSTD_INCLUDE_DIR
     NAMES zstd.h
     HINTS ${zstd_incl_dirs}
 )
 
-message("Ponto 4")
 set(zstd_lib_dirs "/usr/lib" "/usr/local/lib")
 if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
     set(zstd_lib_dirs ${zstd_lib_dirs} "$ENV{PROGRAMFILES}/zstd/lib")
     set(zstd_lib_dirs ${zstd_lib_dirs} "$ENV{PROGRAMDATA}/chocolatey/lib/zstandard/tools/dll")
+    set(zstd_lib_dirs ${zstd_lib_dirs} "$ENV{PROGRAMDATA}/chocolatey/lib/zstandard/tools/static")
 endif()
 
-message("Ponto 5")
 if(ZSTD_LIBRARYDIR)
     list(APPEND zstd_lib_dirs ${ZSTD_LIBRARYDIR})
 endif()
 
-message("Ponto 6")
 find_library(
     ZSTD_LIBRARY
-    NAMES zstd zstdlib libzstd
+    NAMES zstd zstdlib libzstd libzstd_static
     HINTS ${zstd_lib_dirs}
 )
 
-message("Ponto 7")
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ZSTD DEFAULT_MSG
                                   ZSTD_LIBRARY ZSTD_INCLUDE_DIR)
 
-message("Ponto 8")
 mark_as_advanced(ZSTD_INCLUDE_DIR ZSTD_LIBRARY ZSTD_FOUND)
 
-message("Ponto 9")
 set(ZSTD_LIBRARIES ${ZSTD_LIBRARY})
 set(ZSTD_INCLUDE_DIRS ${ZSTD_INCLUDE_DIR})
 
-message("Ponto 10")
 if(ZSTD_FOUND)
     if(NOT TARGET ZSTD::zstd)
         add_library(ZSTD::zstd UNKNOWN IMPORTED)
@@ -85,4 +76,3 @@ if(ZSTD_FOUND)
             INTERFACE_INCLUDE_DIRECTORIES "${ZSTD_INCLUDE_DIR}")
     endif()
 endif()
-message("Ponto 11")
